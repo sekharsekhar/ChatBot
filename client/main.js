1fd943ca2d88d7af81fabdbf14190ded3a7e6d4f
sekhar2004 <152714827+sekharsekhar@users.noreply.github.com>
2025-08-14 11:20:20 +0530

    Update main.js

--- client/main.js
+++ client/main.js
@@ -87,7 +87,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   //fetch data from server ->bot's respose
-  const response = await fetch("http://localhost:5000/", {
+  const response = await fetch("https://chatbot-ex6n.onrender.com", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
